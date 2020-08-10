403b61d20b91653320ad57de856389e5d4909f36
Arthur Frey <afrey@steelcloud.com>
2020-08-10 10:42:03 -0400

    Good Version

---
+++ FirewallApp/ViewModels/ObjectViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Management.Automation;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+
+namespace FirewallApp.ViewModels
+{
+    [MetadataType(typeof(MetaData))]
+    public class ObjectViewModel
+    {
+        public class MetaData : IMetadataProvider<ObjectViewModel>
+        {
+            void IMetadataProvider<ObjectViewModel>.BuildMetadata
+                (MetadataBuilder<ObjectViewModel> p_builder)
+            {
+            }
+        }
+
+        #region Constructors
+
+        protected ObjectViewModel()
+        {
+        }
+
+        public static ObjectViewModel Create()
+        {
+            return ViewModelSource.Create(() => new ObjectViewModel());
+        }
+
+        #endregion
+
+        #region Fields and Properties
+
+        public virtual PSObject SelectedPsObject { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
